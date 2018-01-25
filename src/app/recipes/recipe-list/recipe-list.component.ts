efb9dd2d15184d25299b7eccc141ab17e1624b1c
Prafulla Raut <prafulla.raut@zensar.com>
2018-01-25 14:29:13 +0530

    Add all file

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,16 +9,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[];
 
-  recipes: Recipe[] = [
-    new Recipe('1 Test recipe', '1test recipe contains food', 'http://maxpixel.freegreatpicture.com/static/photo/2x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
-    new Recipe('2 Test recipe', '2test recipe contains food', 'http://maxpixel.freegreatpicture.com/static/photo/2x/Vegetables-Meat-Mushrooms-Meat-Skewer-Gemuesepiess-1440105.jpg'),
-    new Recipe('3 Test recipe', '3test recipe contains food', 'http://maxpixel.freegreatpicture.com/static/photo/2x/Food-Chicken-Menu-Recipe-Bacon-Dinner-Meat-Dish-2428926.jpg')
-  ];
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private recipeService: RecipeService) {
   }
 
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
+  }
 }
